4239acd74191deb73b13769816b1c6716dc70658
SLS\bbhattacharya <bibek.bhattacharya@sharp-ls.eu>
2019-01-05 18:03:52 +0000

    Add tests

--- Reactive/ReactiveTests/StockMonitorTests.cs
+++ Reactive/ReactiveTests/StockMonitorTests.cs
@@ -13,7 +13,7 @@ namespace Tests
         }
 
         [Test]
-        public void TestAlerts()
+        public void TestAlertOnIncrease()
         {
             var ticker = new StockTicker();
             var monitor = new StockMonitor(ticker, 0.3m); // Max ratio: 0.3
@@ -24,14 +24,80 @@ namespace Tests
 
             ticker.UpdateStock("MSFT", 100);
             ticker.UpdateStock("MSFT", 120); // No Alert: Change < Max Change
-            ticker.UpdateStock("INTC", 150);
-            ticker.UpdateStock("MSFT", 165); // Alert: Change = 0.375 > Max Change (0.3)
+            ticker.UpdateStock("MSFT", 165); // Alert: Change  > Max Change
 
             monitor.StockAlert -= handlerAlert;
             
             Assert.AreEqual(1, alerts.Count);
             Assert.IsFalse(alerts.Any(x => x.Symbol == "MSFT" && x.NewPrice == 120));
             Assert.IsTrue(alerts.Any(x => x.Symbol == "MSFT" && x.NewPrice == 165));
+            alerts.Clear();
+        }
+
+        [Test]
+        public void TestAlertOnDecrease()
+        {
+            var ticker = new StockTicker();
+            var monitor = new StockMonitor(ticker, 0.3m); // Max Change : 0.3
+            var alerts = new List<StockAlert>();
+
+            System.EventHandler<StockAlert> handlerAlert = (object sender, StockAlert alert) => { alerts.Add(alert); };
+            monitor.StockAlert += handlerAlert;
+
+            ticker.UpdateStock("MSFT", 195);
+            ticker.UpdateStock("MSFT", 120); // Alert: Change > Max Change
+            ticker.UpdateStock("MSFT", 100); // No Alert: Change < Max Change
+
+            monitor.StockAlert -= handlerAlert;
+            
+            Assert.AreEqual(1, alerts.Count);
+            Assert.IsTrue(alerts.Any(x => x.Symbol == "MSFT" && x.NewPrice == 120));
+            Assert.IsFalse(alerts.Any(x => x.Symbol == "MSFT" && x.NewPrice == 100));
+            
+            alerts.Clear();
+        }
+
+        [Test]
+        public void TestMultipleAlerts()
+        {
+            var ticker = new StockTicker();
+            var monitor = new StockMonitor(ticker, 0.3m); // Max Change : 0.3
+            var alerts = new List<StockAlert>();
+
+            System.EventHandler<StockAlert> handlerAlert = (object sender, StockAlert alert) => { alerts.Add(alert); };
+            monitor.StockAlert += handlerAlert;
+
+            ticker.UpdateStock("MSFT", 100);
+            ticker.UpdateStock("MSFT", 220); // Alert: Change > Max Change
+            ticker.UpdateStock("MSFT", 300); // No Alert: Change < Max Change
+
+            monitor.StockAlert -= handlerAlert;
+            
+            Assert.AreEqual(2, alerts.Count);
+            Assert.IsTrue(alerts.Any(x => x.Symbol == "MSFT" && x.NewPrice == 220));
+            Assert.IsTrue(alerts.Any(x => x.Symbol == "MSFT" && x.NewPrice == 300));
+            
+            alerts.Clear();
+        }
+
+        [Test]
+        public void TestNoAlert()
+        {
+            var ticker = new StockTicker();
+            var monitor = new StockMonitor(ticker, 0.3m); // Max Change : 0.3
+            var alerts = new List<StockAlert>();
+
+            System.EventHandler<StockAlert> handlerAlert = (object sender, StockAlert alert) => { alerts.Add(alert); };
+            monitor.StockAlert += handlerAlert;
+
+            ticker.UpdateStock("MSFT", 100);
+            ticker.UpdateStock("MSFT", 120); // No Alert: Change < Max Change
+            ticker.UpdateStock("MSFT", 150); // No Alert: Change < Max Change
+            ticker.UpdateStock("AAPL", 250); // No Alert: Different symbol
+
+            monitor.StockAlert -= handlerAlert;
+            
+            Assert.AreEqual(0, alerts.Count);
         }
     }
 }
